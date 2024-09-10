84382434124e25fe01faf24e50cfa9198b253be8
fromhaejin <kendo0022@gmail.com>
2024-09-10 22:26:40 +0900

    Update game.js

--- game.js
+++ game.js
@@ -126,8 +126,9 @@ function gameLoop() {
         ctx.font = `${canvas.width * 0.05}px Arial`;
 
         // 최종 점수를 화면 아래쪽으로 더 내리기
-        const scoreYPosition = canavs.height/2 + canvas.height*0.15; // y좌표를 더 밑으로 조정
-        ctx.fillText(`최종 점수: ${score}`, canvas.width / 2 - canvas.width * 0.2, canvas.height / 2 + canvas.height * 0.05);
+        const scoreYPosition = canvas.height / 2 + canvas.height * 0.15; // y 좌표를 더 밑으로 조정
+        ctx.fillText(`최종 점수: ${score}`, canvas.width / 2 - canvas.width * 0.2, scoreYPosition);
+        
         return;
     }
 
